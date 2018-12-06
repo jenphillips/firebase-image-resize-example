e8459e48aa919b2985e23b2b64eb6ff16dc56181
Jennifer Pierce <jpierce@trustedknight.com>
2018-12-06 11:16:05 -0400

    Add firebaseConfig example

---
+++ src/config/firebaseConfig.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "<REDACTED>",
+  authDomain: "closetchartapi.firebaseapp.com",
+  databaseURL: "https://closetchartapi.firebaseio.com",
+  projectId: "closetchartapi",
+  storageBucket: "gs://closetchartapi.appspot.com",
+  messagingSenderId: "<REDACTED>"
+}
